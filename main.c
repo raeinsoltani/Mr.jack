60db6ea20f3ed1babf6996b141245699e1020357
raeinsoltani <raeen.soltani@yahoo.com>
2022-01-20 09:21:34 +0330

    first day progress.

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct map{
+
+/*This is Mr-Jack by Raein Soltani __ Cracking-X
+ * for initial documents on how to start the game please refer to game manual*/
+
+
+struct map_struct{
     int character; //each character will be associated with a certain number.
     int structures; //used to show which structures are in place.
 };
 
+
+struct map_struct* map[13][9];
+
+
+int map_initialization(){
+
+}
+
+
 int main() {
+
     return 0;
 }
